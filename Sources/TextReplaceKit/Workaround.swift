ff54f88c7ecb0eb62687fed103f64b736db22ca1
noppe <noppelabs@gmail.com>
2024-09-30 09:29:48 +0900

    Update Workaround.swift

--- Sources/TextReplaceKit/Workaround.swift
+++ Sources/TextReplaceKit/Workaround.swift
@@ -16,16 +16,17 @@ extension UITextView {
 extension Workaround {
     /// https://developer.apple.com/documentation/uikit/uitextinput/4462768-replace
     /// Before iOS18, UITextView don't implemented replace(range:withAttributedText:).
+    /// Addition note 001
+    ///     - iOS18 replace(withAttributedText:) sometimes
+    ///     `Thread 1: "-[NSNull _defaultLineHeightForUILayout]: unrecognized selector sent to instance 0x1e007fa58"`
     @MainActor
     func replace(
         _ range: UITextRange,
         withAttributedText attributedText: NSAttributedString
     ) {
-        if #available(iOS 18, *) {
-            base.replace(range, withAttributedText: attributedText)
-        } else {
-            base.replace(range, withText: "")
-            base.insertAttributedText(attributedText)
-        }
+        
+        
+        base.replace(range, withText: "")
+        base.insertAttributedText(attributedText)
     }
 }

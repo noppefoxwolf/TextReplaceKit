3b58fc89d9f7ca4b482d6f8ed14211c92b290f8d
noppe <noppelabs@gmail.com>
2024-10-12 00:31:31 +0900

    Update UITextView+.swift

--- Sources/AttachmentReplace/UITextView+.swift
+++ Sources/AttachmentReplace/UITextView+.swift
@@ -2,10 +2,52 @@ import UIKit
 import Extensions
 
 extension UITextView {
+    public enum Granularity {
+        case selectedLine
+        case document
+    }
     
     public typealias AttachmentTransform = (NSTextAttachment) -> NSAttributedString?
     
-    public func replaceAttachment(in range: UITextRange, transform: AttachmentTransform) {
+    public func replaceAttachment(
+        _ transform: AttachmentTransform,
+        granularity: Granularity
+    ) {
+        switch granularity {
+        case .selectedLine:
+            replaceAttachmentLine(
+                transform: transform,
+                at: selectedTextRange?.start ?? endOfDocument
+            )
+        case .document:
+            replaceAttachmentWholeDocument(transform: transform)
+        }
+    }
+
+    func replaceAttachmentLine(transform: AttachmentTransform, at posision: UITextPosition) {
+        let lineRangeStart = tokenizer.position(
+            from: posision,
+            toBoundary: .line,
+            inDirection: .layout(.left)
+        )
+        let lineRangeEnd = tokenizer.position(
+            from: posision,
+            toBoundary: .line,
+            inDirection: .layout(.right)
+        )
+        guard let lineRangeStart, let lineRangeEnd else { return }
+        let lineRange = textRange(from: lineRangeStart, to: lineRangeEnd)
+        guard let lineRange else { return }
+        replaceAttachment(in: lineRange, transform: transform)
+    }
+
+    func replaceAttachmentWholeDocument(transform: AttachmentTransform) {
+        let documentRange = textRange(from: beginningOfDocument, to: endOfDocument)
+        guard let documentRange else { return }
+        replaceAttachment(in: documentRange, transform: transform)
+    }
+    
+    func replaceAttachment(in range: UITextRange, transform: AttachmentTransform) {
         var didChanged: Bool = false
         // 先頭からのrange
         let nsRange = NSRange(range, in: self)

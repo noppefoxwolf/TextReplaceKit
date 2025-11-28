f754203608ab40ef8d16a23cc974197adaca97d6
noppe <noppelabs@gmail.com>
2025-11-28 20:08:20 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TextReplaceKit",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v18)],
     products: [
         .library(
             name: "TextReplaceKit",
